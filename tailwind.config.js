233471ad5ae3b95b03cd6cc6e307aea5e735ddd1
sfauzii <sjahrulfauzii@gmail.com>
2024-11-25 20:13:31 +0700

    navigation

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['poppins']
+      },
+      colors: {
+        biru: '#2447F9',
+        pink: '#F8549F',
+        grey: '#222831'
+      }
+    }
+  },
+  plugins: []
+}
